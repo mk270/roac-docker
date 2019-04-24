8df7f933ee399097949593240c5e0a00c3ff68df
Martin Keegan <martin@no.ucant.org>
2019-04-24 12:58:14 +0100

    make the code generalisable away from OBP spreadsheet format

--- loader/loader.py
+++ loader/loader.py
@@ -15,22 +15,42 @@ from graphqlclient import GraphQLClient
 import os
 import roac_client
 
-def load_obp_books(client, data_file):
-    book_uuids = {}
+class OBPBookLoader:
+    def __init__(self, client, data_file):
+        self.client = client
+        self.data_file = data_file
+        self.book_uuids = {}
+        # self.contributor_uuids
 
-    columns = {
-        "Title": "title",
-        "Subtitle": "subtitle",
-        "DOI prefix": "doiPrefix",
-        "DOI suffix": "doiSuffix",
-        "no of pages": "pageCount",
-        "Cover URL": "cover_url",
-        "Book-page URL": "overview_url"
-    }
+    def load(self):
+        for data in self.get_books():
+            book_uuid = roac_client.save_book(self.client, data,
+                                              "Open Book Publishers")
+            self.book_uuids[data["row_id"]] = book_uuid
+
+        contributors = frozenset([ (c[0], c[1])
+                                   for c in self.get_contributors() ])
+
+        self.contributor_uuids = roac_client.save_contributors(self.client,
+                                                               contributors)
+
+        for contribution in self.get_contributions():
+            roac_client.save_contribution(self.client, *contribution)
+
+        self.get_publications()
 
     # note duplicated fn below
-    def get_books():
-        r = csv.DictReader(open(data_file))
+    def get_books(self):
+        columns = {
+            "Title": "title",
+            "Subtitle": "subtitle",
+            "DOI prefix": "doiPrefix",
+            "DOI suffix": "doiSuffix",
+            "no of pages": "pageCount",
+            "Cover URL": "cover_url",
+            "Book-page URL": "overview_url"
+        }
+        r = csv.DictReader(open(self.data_file))
         row_id = 0
         for row in r:
             data = dict([ (v, row[k]) for k, v in columns.items() ])
@@ -42,11 +62,7 @@ def load_obp_books(client, data_file):
             data['row'] = row
             yield data
 
-    for data in get_books():
-        book_uuid = roac_client.save_book(client, data, "Open Book Publishers")
-        book_uuids[data["row_id"]] = book_uuid
-
-    def contributors_from_row(row):
+    def contributors_from_row(self, row):
         for contributor in range(0, 6):
             offset = contributor * 7 + 5
             first_name = row[offset]
@@ -56,19 +72,16 @@ def load_obp_books(client, data_file):
                 continue
             yield (first_name, last_name, role)
 
-    def get_contributors():
-        r = csv.reader(open(data_file))
+    def get_contributors(self):
+        r = csv.reader(open(self.data_file))
         for row in r:
             if row[0] in ["", "Title"]:
                 continue
-            for c in contributors_from_row(row):
+            for c in self.contributors_from_row(row):
                 yield c
 
-    contributors = frozenset([ (c[0], c[1]) for c in get_contributors() ])
-    contributor_uuids = roac_client.save_contributors(client, contributors)
-
-    def get_books_no_dict():
-        r = csv.reader(open(data_file))
+    def get_books_no_dict(self):
+        r = csv.reader(open(self.data_file))
         row_id = 0
         for row in r:
             if row[0] in ["", "Title"]:
@@ -76,19 +89,16 @@ def load_obp_books(client, data_file):
             row_id += 1
             yield row_id, row
 
-    def get_contributions():
-        for row_id, row in get_books_no_dict():
-            for c in contributors_from_row(row):
+    def get_contributions(self):
+        for row_id, row in self.get_books_no_dict():
+            for c in self.contributors_from_row(row):
                 key = (c[0], c[1])
-                c_uuid = contributor_uuids[key]
-                b_uuid = book_uuids[row_id]
+                c_uuid = self.contributor_uuids[key]
+                b_uuid = self.book_uuids[row_id]
                 role = c[2]
                 yield (b_uuid, c_uuid, role)
 
-    for contribution in get_contributions():
-        roac_client.save_contribution(client, *contribution)
-
-    def publications_from_row(row):
+    def publications_from_row(self, row):
         for pub in range(0, 5):
             offset = pub * 3 + 47
             pub_format = row[offset].lower().strip()
@@ -99,13 +109,17 @@ def load_obp_books(client, data_file):
                 continue
             yield (pub_format, pub_isbn)
 
-    def get_publications():
-        for row_id, row in get_books_no_dict():
-            for pub_format, pub_isbn in publications_from_row(row):
-                roac_client.save_publication(client, book_uuids[row_id],
-                                 pub_format, pub_isbn)
+    def get_publications(self):
+        for row_id, row in self.get_books_no_dict():
+            for pub_format, pub_isbn in self.publications_from_row(row):
+                roac_client.save_publication(self.client,
+                                             self.book_uuids[row_id],
+                                             pub_format,
+                                             pub_isbn)
 
-    get_publications()
+def load_obp_books(client, data_file):
+    book_loader = OBPBookLoader(client, data_file)
+    book_loader.load()
 
 def run(client, data_file):
     roac_client.save_publishers(client)
