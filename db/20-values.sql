1c7ec862b4404806cf6766a4abb619916e5c4bc1
Martin Keegan <martin@no.ucant.org>
2019-06-14 18:18:42 +0100

    convert remaining code lists to csv

--- db/20-values.sql
+++ db/20-values.sql
@@ -4,18 +4,11 @@ copy language_code (lang_code)
 copy format (format_name)
   from '/docker-entrypoint-initdb.d/formats.csv' with csv;
 
-insert into contributor_role (role_name) values ('author');
-insert into contributor_role (role_name) values ('editor');
-insert into contributor_role (role_name) values ('illustrator');
-insert into contributor_role (role_name) values ('introduction');
-insert into contributor_role (role_name) values ('foreword');
-insert into contributor_role (role_name) values ('preface');
-insert into contributor_role (role_name) values ('translator');
-insert into contributor_role (role_name) values ('music editor');
+copy contributor_role (role_name)
+  from '/docker-entrypoint-initdb.d/contributor_roles.csv' with csv;
 
-insert into detail (detail_id, detail_name) values ('cover_url', 'Cover URL');
-insert into detail (detail_id, detail_name) values ('overview_url', 'Overview URL');
-insert into detail (detail_id, detail_name) values ('doi', 'DOI');
-insert into detail (detail_id, detail_name) values ('short_blurb', 'Short blurb');
+copy detail (detail_id, detail_name)
+  from '/docker-entrypoint-initdb.d/details.csv' with csv;
 
-copy currency (currency_code) from '/docker-entrypoint-initdb.d/iso4217.csv' with csv;
+copy currency (currency_code)
+  from '/docker-entrypoint-initdb.d/iso4217.csv' with csv;
