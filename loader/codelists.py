6094baf58f69ec2b4a4a75b512b1874ebb6476ef
Martin Keegan <martin@no.ucant.org>
2019-06-14 20:00:52 +0100

    add missing file

---
+++ loader/codelists.py
@@ -0,0 +1,8 @@
+
+# FIXME: should pull this out of the db dir
+
+def get_currencies():
+    return ["GBP", "USD", "AUD", "EUR", "CAD"]
+
+def get_formats():
+    return ["paperback", "hardback", "pdf", "epub", "mobi", "html", "xml"]
